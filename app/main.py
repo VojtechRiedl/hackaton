4508e203232d0498f39586a7f06dab3f7704f569
Tomas Kroupa <tom@bagros.eu>
2025-03-07 23:16:21 +0100

    cors nebo tak neco

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api.kraje.router import kraje_router
 app = FastAPI(
     title="SKIBIDI"
 )
 
+cors = {
+    "origins": ["*"],
+    "methods": ["*"],
+    "allowed_headers": ["*"]
+}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=cors["origins"],
+    allow_methods=cors["methods"],
+    allow_headers=cors["allowed_headers"],
+)
 
 app.include_router(kraje_router)
