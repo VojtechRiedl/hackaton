5122a99c1295f8f1527392bb5e2c55b26a58452a
Tomas Kroupa <tom@bagros.eu>
2025-03-08 01:30:02 +0100

    uspesnost

--- app/core/api/kraje/router.py
+++ app/core/api/kraje/router.py
@@ -14,7 +14,3 @@ kraje_router = APIRouter(prefix="/kraje", tags=["kraje"])
 @kraje_router.get("/{kraj_id}/okresy", response_model=list[Okres])
 def get_okresy(kraj_id: str, db: Session = Depends(get_db)):
     kraje_manager.get_okresy_from_kraj(kraj_id, db)
-
-    
-
-
