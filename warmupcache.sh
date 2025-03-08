d479dca6ca9a79ccb2c09fbc87d514994c78b97e
Tomas Kroupa <tom@bagros.eu>
2025-03-08 07:59:39 +0100

    warmupcache

---
+++ warmupcache.sh
@@ -0,0 +1,14 @@
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/2312
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/2212
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/2112
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/2012
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/1912
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/1812
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/1712
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/1612
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/1512
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/1412
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/1312
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/1212
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/1112
+curl -I https://hackujapi.ladislavpokorny.cz/school/finance/1012
